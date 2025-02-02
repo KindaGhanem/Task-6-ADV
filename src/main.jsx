30adcb53af49144ec4e7dd70a60563dd223e0090
Kinda Ghanem <ghanemkinda0@gmail.com>
2025-02-02 20:55:41 +0300

    blog

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter  basename="/Task-6-ADV">
         <App />
       </BrowserRouter>
     </Provider>
