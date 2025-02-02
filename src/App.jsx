2bccf6e231991420a96a92914736d67aa0ac752c
Kinda Ghanem <ghanemkinda0@gmail.com>
2025-02-02 20:59:15 +0300

    blog

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
       <NavBar name={'Your Name'} logoDesc='logo' items={navItems} btn='login' />
       <Routes>
         {/* <Route path="/" element={<Navigate to="/Task-6-ADV/" />} /> */}
-        <Route path='/Task-6-ADV/' element={<Home />}></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path='/Details/:id' element={<Details />}></Route>
         <Route path='/Newsletter' element={<NewsLetter />}></Route>
       </Routes>
